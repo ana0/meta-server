055c9b805c2296d021508567c7b3ab7276b30d2c
ana0 <sarah.anne.friend@gmail.com>
2023-07-20 22:41:04 +0200

    Update seeders

--- src/database/seeders/20230713155150-create-mirror-metadata.js
+++ src/database/seeders/20230713155150-create-mirror-metadata.js
@@ -4,7 +4,8 @@ module.exports = {
       {
         createdAt: new Date(),
         updatedAt: new Date(),
-        description: "A mirror requires patient labour and attention",
+        description:
+          "Mirror Piece is an interactive performance commissioned as part of Echoes 2023 by the Haus Der Kunst in München and orchestrated by Sarah Friend. In a future iteration, this token will be activated and transform in a collective dialogue.",
         image:
           "https://isthisa.computer/ipfs/QmZDD2vSxnBdLf6PgEmLqodG5LnSsXvp8WTWy7MRwQYJzk/mirror-piece-square.jpg",
       },
